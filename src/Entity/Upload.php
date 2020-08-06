af83dbb8fdcb3dcaafbc02776ae5bdbb3525f599
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-08-06 11:22:41 +0200

    upload ok

--- src/Entity/Upload.php
+++ src/Entity/Upload.php
@@ -3,13 +3,20 @@
 namespace App\Entity;
 
 use App\Repository\UploadRepository;
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=UploadRepository::class)
+ * @Vich\Uploadable
  */
 class Upload
 {
+    const MAX_SIZE="2000k";
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -18,14 +25,20 @@ class Upload
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $identity;
-
-    /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $status;
+
+    /**
+     * @Vich\UploadableField(mapping="status_file", fileNameProperty="status")
+     * @var File|null
+     * @Assert\File(maxSize = Upload::MAX_SIZE,
+     *     maxSizeMessage="Le fichier est trop gros  ({{ size }} {{ suffix }}),
+     * il ne doit pas dépasser {{ limit }} {{ suffix }}",
+     *     mimeTypes = {"application/pdf", "application/x-pdf"},
+     *     mimeTypesMessage = "Veuillez entrer un type de fichier PDF.")
+     */
+    private $statusFile;
 
     /**
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="uploads")
@@ -33,21 +46,15 @@ class Upload
      */
     private $user;
 
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @var DateTime|null
+     */
+    private $updatedAt;
+
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getIdentity(): ?string
-    {
-        return $this->identity;
-    }
-
-    public function setIdentity(string $identity): self
-    {
-        $this->identity = $identity;
-
-        return $this;
     }
 
     public function getStatus(): ?string
@@ -61,6 +68,21 @@ class Upload
 
         return $this;
     }
+
+
+    public function setStatusFile(File $status = null)
+    {
+        $this->statusFile = $status;
+        if ($status) {
+            $this->updatedAt = new DateTime('now');
+        }
+    }
+
+    public function getStatusFile(): ?File
+    {
+        return $this->statusFile;
+    }
+
 
     public function getUser(): ?User
     {
---
+++ src/Form/UploadType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Upload;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class UploadType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('statusFile', VichFileType::class, [
+                'label' => 'Fichier à télécharger',
+                'required' => false,
+                'allow_delete' => true,
+                'download_uri' => true,
+            ])
+            ->remove('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Upload::class,
+        ]);
+    }
+}
