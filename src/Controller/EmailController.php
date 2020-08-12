e0ea048ddb51ff8b63246bd2c52a67399aa8c8be
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-08-12 09:16:48 +0200

    ajout logo

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -3,11 +3,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Person;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
 
 class EmailController extends AbstractController
 {
+    /**
+     * @Route("/email", name = "email")
+     */
+    public function email(Request $request, MailerInterface $mailer):Response
+    {
+        $person = $this->getDoctrine()
+            ->getRepository(Person::class)
+            ->findAll();
+
+
+        $email = (new Email())
+            ->from("test@test.com")
+            ->to('wild-circus@wild.com')
+            ->subject('test')
+            ->html($this->renderView('email/order_confirmation.html.twig', [
+                'person' => $person
+            ]));
+        $mailer->send($email);
+        return $this->redirectToRoute('home');
+    }
+
+
+
     /**
      * @Route("/email/paiement", name="paiement")
      */
