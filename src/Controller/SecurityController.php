7168454763223a3a36303c02d53097218ab4e872
Yowit <yowitweb@gmail.com>
2020-08-16 22:21:53 +0000

    revert login pop

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,24 +12,41 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/connexion", name="login")
+     /**
+     * @Route("/login", name="app_login")
      * @param AuthenticationUtils $authenticationUtils
      * @return Response
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+        
     }
+    // /**
+    //  * @Route("/connexion", name="login")
+    //  * @param AuthenticationUtils $authenticationUtils
+    //  * @return Response
+    //  */
+    // public function login(AuthenticationUtils $authenticationUtils): Response
+    // {
+    //     // if ($this->getUser()) {
+    //     //     return $this->redirectToRoute('target_path');
+    //     // }
+
+    //     // get the login error if there is one
+    //     $error = $authenticationUtils->getLastAuthenticationError();
+    //     // last username entered by the user
+    //     $lastUsername = $authenticationUtils->getLastUsername();
+
+    //     return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    // }
 
 
     /**
