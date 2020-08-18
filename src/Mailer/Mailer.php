bbd4cb05339f0ebb870d37823f55121ece6a9d11
akouke <68338410+akouke@users.noreply.github.com>
2020-08-18 15:00:13 +0000

    Update Mailer.php

--- src/Mailer/Mailer.php
+++ src/Mailer/Mailer.php
@@ -41,8 +41,8 @@ class Mailer
             
             ]);
         $message = (new \Swift_Message())
-        ->setSubject('IZYcontrat Confirmation Email ')
-        ->setFrom($this->emailFrom)
+        ->setSubject('IZY contrat Confirmation Email ')
+        ->setFrom('contact@izy-contrat.fr')
         ->setTo($person->getUser()->getEmail())
         ->setBody($body, 'text/html');
         //->attach(Swift_Attachment::fromPath('my-document.pdf'))
